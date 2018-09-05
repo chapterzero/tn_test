d43e02a71face12dabcef4fc4c8b557a0618f1e8
Yusuf Irwandi <yusuf.chapterzero@gmail.com>
2018-09-05 18:10:12 +0700

    Route - refactor

--- server/router.go
+++ server/router.go
@@ -7,8 +7,11 @@ import (
 
 func CreateRouter() *mux.Router {
 	r := mux.NewRouter()
+	registerRoutes(r)
+	return r
+}
+
+func registerRoutes(r *mux.Router) {
 	r.HandleFunc("/api/customer", handlers.PostCustomerHandler).Name("PostCustomer").Methods("POST")
 	r.HandleFunc("/api/customer_type", handlers.GetCustomerTypeHandler).Name("GetCustomerType").Methods("GET")
-
-	return r
 }
